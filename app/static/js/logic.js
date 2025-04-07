a86fdead247957b72a3140f6acab96b2568f6f8e
gartles20 <thomasgartley33@gmail.com>
2025-04-07 19:22:31 -0400

    added model pkl and supporting files

---
+++ app/static/js/logic.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    console.log("Page Loaded");
+
+    $("#predict").click(function() {
+        predictions();
+    });
+});
+
+// Call Flask API endpoint
+function predictions() {
+    // Get form values
+    var distance_from_home = parseFloat($("#distance_from_home").val());
+    var distance_from_last_transaction = parseFloat($("#distance_from_last_transaction").val());
+    var ratio_to_median_purchase_price = parseFloat($("#ratio_to_median_purchase_price").val());
+    var repeat_retailer = $("#repeat_retailer").is(":checked") ? 1.0 : 0.0;
+    var used_chip = $("#used_chip").is(":checked") ? 1.0 : 0.0;
+    var used_pin_number = $("#used_pin_number").is(":checked") ? 1.0 : 0.0;
+    var online_order = $("#online_order").is(":checked") ? 1.0 : 0.0;
+
+    // Create payload
+    var payload = {
+        "distance_from_home": distance_from_home,
+        "distance_from_last_transaction": distance_from_last_transaction,
+        "ratio_to_median_purchase_price": ratio_to_median_purchase_price,
+        "repeat_retailer": repeat_retailer,
+        "used_chip": used_chip,
+        "used_pin_number": used_pin_number,
+        "online_order": online_order
+    };
+
+    // POST request to backend
+    $.ajax({
+        type: "POST",
+        url: "/predictions",
+        contentType: 'application/json;charset=UTF-8',
+        data: JSON.stringify({ "data": payload }),
+        success: function(returnedData) {
+            console.log("Prediction returned:", returnedData);
+            //{ok: true, prediction: '0.03'}
+            var prob = parseFloat(returnedData.prediction);
+
+            $("#output").text(` ${(prob )}%`);
+            // if (prob > 0.5) {
+            //     $("#output").text(`⚠️ Likely fraud — Risk score: ${(prob * 100).toFixed(2)}%`);
+            // } else {
+            //     $("#output").text(`✅ Low fraud risk — Risk score: ${(prob * 100).toFixed(2)}%`);
+            // }
+
+            // Optional: call chart or other visual functions
+            // buildDonut(prob);
+        },
+
+        
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert("Status: " + textStatus);
+            alert("Error: " + errorThrown);
+        }
+    });
+}
